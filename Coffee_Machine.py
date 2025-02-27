13bea35a1be91246e5224116cf7450ac87bd4aab
Tevyn <tevyn.payne@gmail.com>
2025-02-27 12:48:03 -0600

    Created a function called check_customer_money that compares the amount the customer put in to the amount needed to purchase the drink. If enough was entered, checked if change is due, otherwise prints an enjoy statement

--- Coffee_Machine.py
+++ Coffee_Machine.py
@@ -36,15 +36,41 @@ def take_money():
     dimes = int(input("How many dimes do you have? "))
     nickels = int(input("How many nickels do you have? "))
     pennies = int(input("How many pennies do you have? "))
-    quarters *= 25
-    dimes *= 10
-    nickels *= 5
+    quarters *= .25
+    dimes *= .10
+    nickels *= .05
+    pennies *= .01
 
     amount = quarters + dimes + nickels + pennies/ 100
+    amount = round(amount, 2)
+    check_customer_money(amount)
     return amount
+
+#TODO: access resources and subtract the amounts of the chosen drink from resources
+def set_resources(coffee):
+    if coffee == "espresso":
+        resources["water"] -= MENU[coffee]["ingredients"]["water"]
+        resources["coffee"] -= MENU[coffee]["ingredients"]["coffee"]
+    else:
+        resources["water"] -= MENU[coffee]["ingredients"]["water"]
+        resources["milk"] -= MENU[coffee]["ingredients"]["milk"]
+        resources["coffee"] -= MENU[coffee]["ingredients"]["coffee"]
+    return resources
+
+#TODO: Create a function that takes in the money the customer puts in and subtracts it from the cost of the coffee. Check if the customer put in the correct or more than required amount of money
+def check_customer_money(a):
+    if a >= MENU[coffee_choice]["cost"]:
+        if a == MENU[coffee_choice]["cost"]:
+            print("Enjoy your coffee!")
+        else:
+            print(f"Your change is ${(MENU[coffee_choice]["cost"] - a)*-1:.2}")
+            print("Enjoy your coffee")
+    else:
+        print(f"You do not have enough money. Returning ${a:.2}")
 
 
 coffee_choice = input("What would you like? (espresso/latte/cappuccino):").lower()
-print(MENU[coffee_choice])
-print(take_money())
+print(f"Your {coffee_choice} is ${MENU[coffee_choice]["cost"]:.2f}")
+take_money()
+
 
